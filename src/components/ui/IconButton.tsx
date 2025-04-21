529a19f88eae571c970afacc0fd010c17df3cd86
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-20 23:35:21 -0300

    add: style

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes } from "react";
 
 export default function IconButton (props:HTMLAttributes<HTMLButtonElement>) {
     return (
-        <button {...props} type="button" className={`bg-blue-700 text-slate-100 rounded-full p-2 duration-300 ${props.className}`}>
+        <button {...props} type="button" className={`bg-blue-700 text-slate-100 rounded-full p-2 duration-300 hover:bg-blue-500 ${props.className}`}>
             { props.children }
         </button>
     );
